457f982eada4b56c6763703b1c90ff5285a860df
wildething96 <85587609+wildething96@users.noreply.github.com>
2022-02-03 11:54:15 +0000

    Changed to required

--- src/film/filmModel.js
+++ src/film/filmModel.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const filmSchema = mongoose.Schema({
   name: {
     type: String,
-    require: true,
+    required: true,
   },
   actor: {
     type: String,
